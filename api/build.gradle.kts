631401c7239457843c3d6364afd1c91cf8033b0b
beatdjam <beat3526@gmail.com>
2023-01-24 01:00:28 +0900

    Spring Bootのアプリケーション、DBが立ち上がるところまで

---
+++ api/build.gradle.kts
@@ -0,0 +1,47 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "3.0.2"
+	id("io.spring.dependency-management") version "1.1.0"
+	id("com.arenagod.gradle.MybatisGenerator") version "1.4"
+	kotlin("jvm") version "1.7.22"
+	kotlin("plugin.spring") version "1.7.22"
+}
+
+group = "com.beatdjam.book.manager"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:3.0.0")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation("org.mybatis.dynamic-sql:mybatis-dynamic-sql:1.2.1")
+	implementation("mysql:mysql-connector-java:8.0.23")
+	mybatisGenerator("org.mybatis.generator:mybatis-generator-core:1.4.0")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "17"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+mybatisGenerator {
+	verbose = true
+	configFile = "${projectDir}/src/main/resources/generatorConfig.xml"
+}
